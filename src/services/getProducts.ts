96efa8615dba9ea2e0e0fe49ed932b565097b1dd
Benhur <benhur_zambiasi@hotmail.com>
2022-10-24 03:54:27 -0300

    alteração em url

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,5 +1,5 @@
 export async function getProducts() {
-  const result = await fetch(`${process.env.NEXT_PUBLIC_ROUTE_API}/api/staticData`);
+  const result = await fetch(`${process.env.NEXT_PUBLIC_ROUTE_API}/staticData`);
   const data = await result.json();
 
   return data && JSON.parse(data);
